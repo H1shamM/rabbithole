ece339f450630d3d487578400ac46174b897d49c
H1shamM <hisham@example.com>
2026-06-08 04:02:06 +0300

    fix(ui): remove references to deleted index.css

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -4,7 +4,7 @@
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./globals.css";
 import App from "./App.tsx";
 import { ToastProvider } from "./contexts/ToastContext.tsx";
 
